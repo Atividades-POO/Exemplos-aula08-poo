f065d6062a4256d5dcb783c354c8060aa3435dfd
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-08 14:55:39 -0300

    Create cliente.py

---
+++ cliente.py
@@ -0,0 +1,15 @@
+#
+# Autores:
+# Michel Silva
+# Emanuel Frank
+# Carlos Eduardo
+# data: 08/07/2022
+#
+# importa a classe Pessoa02 como P2
+from pessoa02 import Pessoa02 as P2
+
+# classe cliente (subclasse)
+class Cliente(P2):
+
+  def comprar(self):
+    print(f"O objeto {self.nomeClasse} comprando algo!")
