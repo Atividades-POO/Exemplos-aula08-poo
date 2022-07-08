a163247da23df5efcf5407a033c239718e66254c
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-08 14:24:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,8 @@
 # Carlos Eduardo
 # data: 08/07/2022
 #
+# importar as classes
+from aluno import Aluno # importa a classe Aluno (subclasse)
+
+# instanciar a classe Aluno (subclasse)
+a1 = Aluno("Michel Silva", 20, "12345", 10, 10) # instancia o objeto a1 da classe Aluno (subclasse)
